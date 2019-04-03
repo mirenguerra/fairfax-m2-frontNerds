228a0b0f1cff32bfa820297ab55091b5ac8bfe7d
valerialp <valeria.lorentep@gmail.com>
2019-04-03 23:51:36 +0200

    add const and listener

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const inputEmailEl = document.querySelector('#email');
+const linkEmailEl = document.querySelector('.email').href;
+console.log(linkEmailEl);
+
+function handlerEmail(event){
+    const inputValue = inputEmailEl.value;
+    linkEmailEl = inputValue;
+    console.log(linkEmailEl);
+}
+
+
+inputEmailEl.addEventListener('keyup', handlerEmail );
