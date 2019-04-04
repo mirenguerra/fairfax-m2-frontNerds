fe9ca9b6630bff45d6fe13d4678a57f4ed970fe4
Irene Fuente <ifuenteromero@gmail.com>
2019-04-04 13:51:17 +0200

    Add preview telephone

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputTel = document.getElementById('phone');
+const linkTel = document.getElementById('phone-link');
+
+console.log(name);
+
+function changeLinkTel(){
+    
+    linkTel.setAttribute('href','tel: +34'+ inputTel.value);
+    
+    console.log(linkTel.href);
+}
+
+
+
+inputTel.addEventListener('keyup',changeLinkTel);
+
