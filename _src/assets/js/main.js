b0337b8d5c20045ef645c040576995d90e13e86f
Irene Fuente <ifuenteromero@gmail.com>
2019-04-05 13:42:21 +0200

    collapsable- sections with some errors to check

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -69,5 +69,49 @@ setGitHub.addEventListener('keyup', getGitHub);
 
 // COLLAPSABLE SECTIONS
 
-const arrowDesign = document.querySelector('#arrow-design');
-const sectionDesign = document.querySelector('');
+//SECTION DESIGN
+
+const arrowIconDesign = document.querySelector('#arrow-design');
+const sectionDesign = document.querySelector('#section-design');
+const arrowButtonDesign =document.querySelector('#ButtonDesign');
+function hideDesignSection(){
+    sectionDesign.classList.toggle('design');
+    sectionDesign.classList.toggle('hidden');
+    arrowIconDesign.classList.toggle('fa-chevron-up');
+    arrowIconDesign.classList.toggle('fa-chevron-down');
+}
+
+{/* <i class="fas fa-chevron-up"></i> */}
+
+arrowButtonDesign.addEventListener('click',hideDesignSection);
+
+//SECTION FILL THE GAP
+const arrowIconFill = document.querySelector('#arrow-fillTheGap');
+const sectionFill = document.querySelector('.fillTheGap');
+const arrowButtonFill =document.querySelector('#ButtonFill');
+
+
+function hideFillSection(){
+
+    sectionFill.classList.toggle('hidden');
+    arrowIconFill.classList.toggle('fa-chevron-up');
+    arrowIconFill.classList.toggle('fa-chevron-down');
+}
+
+arrowButtonFill.addEventListener('click',hideFillSection);
+
+//SECTION SHARE
+
+const arrowIconShare = document.querySelector('#arrow-share');
+const sectionShare = document.querySelector('#section-share');
+const arrowButtonShare =document.querySelector('#ButtonShare');
+
+function hideShareSection(){
+
+    sectionShare.classList.toggle('hidden');
+    arrowIconShare.classList.toggle('fa-chevron-up');
+    arrowIconShare.classList.toggle('fa-chevron-down');
+}
+
+
+arrowButtonShare.addEventListener('click',hideShareSection);
