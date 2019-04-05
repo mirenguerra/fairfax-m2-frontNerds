63cb59a4214a810e5bb976f1f1391baa701c41ad
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-05 10:16:09 +0200

    resolve indentation problems js and add colors variables on css

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,11 +5,11 @@ const setName = document.querySelector('#name');
 const cardUserName = document.querySelector('.card-user__name');
 
 function getUserName(){
-    const userName = setName.value;
-    cardUserName.innerHTML = userName;  
-    if (userName ===''){
-        cardUserName.innerHTML = 'User name';
-    }
+  const userName = setName.value;
+  cardUserName.innerHTML = userName;
+  if (userName ===''){
+    cardUserName.innerHTML = 'User name';
+  }
 }
 
 setName.addEventListener('keyup', getUserName);
@@ -19,11 +19,11 @@ const setJob = document.querySelector('#job');
 const cardUserJob = document.querySelector('.card-user__position');
 
 function getUserJob(){
-    const userJob = setJob.value;
-    cardUserJob.innerHTML = userJob;
-    if (userJob ===''){
-        cardUserJob.innerHTML = 'User job';
-    }
+  const userJob = setJob.value;
+  cardUserJob.innerHTML = userJob;
+  if (userJob ===''){
+    cardUserJob.innerHTML = 'User job';
+  }
 }
 
 setJob.addEventListener('keyup', getUserJob);
@@ -34,11 +34,10 @@ let linkEmailEl = document.querySelector('.email');
 
 
 function handlerEmail(event){
-    const inputValue = inputEmailEl.value;
-    linkEmailEl.href = `mailto:${inputValue}`;
-    console.log(linkEmailEl);
+  const inputValue = inputEmailEl.value;
+  linkEmailEl.href = `mailto:${inputValue}`;
+//   console.log(linkEmailEl);
 }
-
 
 inputEmailEl.addEventListener('keyup', handlerEmail);
 
@@ -47,22 +46,22 @@ inputEmailEl.addEventListener('keyup', handlerEmail);
 const inputTel = document.getElementById('phone');
 const linkTel = document.getElementById('phone-link');
 function changeLinkTel(){
-    linkTel.setAttribute('href','tel: +34'+ inputTel.value);
+  linkTel.setAttribute('href','tel: +34'+ inputTel.value);
 }
 
 inputTel.addEventListener('keyup',changeLinkTel);
 
 // GITHUB
 const setGitHub = document.querySelector('#gitHub');
-const cardUserGitHub = document.querySelector('#gitHub-link')
-const iconStyle = document.querySelector('#gitHubEl')
+const cardUserGitHub = document.querySelector('#gitHub-link');
+const iconStyle = document.querySelector('#gitHubEl');
 
 function getGitHub(){
-    const userGitHub = setGitHub.value;
-    cardUserGitHub.href =`https://github.com/${userGitHub}`;
-    if (userGitHub !== ''){
-        iconStyle.classList.add('card__item-list-default');
-    }
+  const userGitHub = setGitHub.value;
+  cardUserGitHub.href =`https://github.com/${userGitHub}`;
+  if (userGitHub !== ''){
+    iconStyle.classList.add('card__item-list-default');
+  }
 }
 
 setGitHub.addEventListener('keyup', getGitHub);
@@ -75,27 +74,23 @@ const linkLnkdIn = document.querySelector('.card__link-linkedin');
 inputLnkdIn.addEventListener('keyup', handleInputKeyup);
 
 function handleInputKeyup(event) {
-    const linkedinInput = inputLnkdIn;
-    const inputValueLnkdIn = linkedinInput.value;
-    linkLnkdIn.href = inputValueLnkdIn;
-        
+  const linkedinInput = inputLnkdIn;
+  const inputValueLnkdIn = linkedinInput.value;
+  linkLnkdIn.href = inputValueLnkdIn;
 }
 
-//PALETES COLORS
-
-const inputRed = document.getElementById('palette--red-orange');
-//const userName = document.querySelector('.card__container');////borde del container
+//PALETTES COLORS
 const userName = document.querySelector('.card-user');
-const contactList = document.querySelector('.card-sm__container');////borde del container
+const inputRed = document.getElementById('palette--red-orange');
 const inputGrey = document.getElementById('palette--blue-gray');
 const inputGreen = document.getElementById ('palette--blue-green');
+const contactList = document.querySelector('.card-sm__container');
 
 function changePaletteRed(){
   userName.classList.remove('gray-palette','green-palette','borderLeftGreen','borderLeftGray');
   userName.classList.add('red-palette', 'borderLeftRed');
   contactList.classList.remove('gray-palette','green-palette');
   contactList.classList.add('red-palette');
-
 }
 
 function  changePaletteGray(){
@@ -103,7 +98,6 @@ function  changePaletteGray(){
   userName.classList.add('gray-palette','borderLeftGray');
   contactList.classList.remove('green-palette','red-palette');
   contactList.classList.add('gray-palette');
-
 }
 
 function changePaletteGreen(){
@@ -111,7 +105,6 @@ function changePaletteGreen(){
   userName.classList.add('green-palette', 'borderLeftGreen');
   contactList.classList.remove('gray-palette','red-palette');
   contactList.classList.add('green-palette');
-
 }
 
 inputRed.addEventListener('click', changePaletteRed);
