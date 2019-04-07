cb8ba76433cee034caec7188f7269c4f26b21fd0
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-07 23:54:13 +0200

    create function circles for github linkeddin and phone li/inputs

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -163,15 +163,15 @@ inputGrey.addEventListener('click', changePaletteGray);
 inputGreen.addEventListener('click', changePaletteGreen);
 
 
-//FUNCTIONS CIRCLES
+//FUNCTIONS CIRCLES ON-OFF
 
 //EMAIL
 
 function emailCircle(){
-  const mailFill = document.querySelector('#email-li');
-  const mailInputContent = document.querySelector('#email');
+  const mailFill = document.getElementById('email-li');//mainpreview
+  const mailInputContent = document.getElementById('email');//fillgap
 
-  if( mailInputContent.value === ''){
+  if(mailInputContent.value === ''){
     mailFill.classList.add('card__item-list-no-border');
     mailFill.classList.remove('card__item-list');
   }else {
@@ -180,7 +180,59 @@ function emailCircle(){
   }
 
 }
-const mailInput = document.querySelector('#email');
+const mailInput = document.getElementById('email');
 mailInput.addEventListener('keyup',emailCircle);
 
+//PHONE
 
+function phoneCircle(){
+  const phoneFill = document.getElementById('phone-li');
+  const phoneInputContent = document.getElementById('phone');
+
+  if(phoneInputContent.value === ''){
+    phoneFill.classList.add('card__item-list-no-border');
+    phoneFill.classList.remove('card__item-list');
+  }else {
+    phoneFill.classList.remove('card__item-list-no-border');
+    phoneFill.classList.add('card__item-list');
+  }
+}
+
+const phoneInput = document.getElementById('phone');
+phoneInput.addEventListener('keyup', phoneCircle);
+
+//LINKEDIN
+
+function linkedinCircle(){
+  const linkedinFill = document.getElementById('linkedin-li');
+  const linkedinInputContent = document.getElementById('linkedin');
+
+  if(linkedinInputContent.value === ''){
+    linkedinFill.classList.add('card__item-list-no-border');
+    linkedinFill.classList.remove('card__item-list');
+  }else {
+    linkedinFill.classList.remove('card__item-list-no-border');
+    linkedinFill.classList.add('card__item-list');
+  }
+}
+
+const linkedinInput = document.getElementById('linkedin');
+linkedinInput.addEventListener('keyup', linkedinCircle);
+
+//GITHUB
+
+function githubCircle(){
+  const githubFill =document.getElementById('githubEl');
+  const githubInputContent = document.getElementById('gitHub');
+
+  if(githubInputContent.value ===''){
+    githubFill.classList.add('card__item-list-no-border');
+    githubFill.classList.remove('card__item-list');
+  }else {
+    githubFill.classList.remove('card__item-list-no-border');
+    githubFill.classList.add('card__item-list');
+  }
+}
+
+const githubInput =document.getElementById ('gitHub');
+githubInput.addEventListener('keyup', githubCircle);
