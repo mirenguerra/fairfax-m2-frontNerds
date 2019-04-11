1e972dbe0680b1f857cf2dd894aa2c07e5fbcfb5
valerialp <valeria.lorentep@gmail.com>
2019-04-11 21:02:29 +0200

    add pop up reset button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -270,3 +270,25 @@ function githubCircle(){
 
 const githubInput = document.getElementById('gitHub');
 githubInput.addEventListener('keyup', githubCircle);
+
+//Resert
+const resertEl = document.querySelector('.reset__btn');
+const buttonResetYEl = document.querySelector('.btn-resetY');
+const buttonResetNEl = document.querySelector('.btn-resetN');
+const containerAlertEl = document.querySelector('.alert');
+
+
+function alertReset(){
+  containerAlertEl.classList.add('alert-delete');
+}
+
+function keepInfo(){
+  containerAlertEl.classList.remove('alert-delete');
+}
+
+function handlerButtonResetClick(){
+
+}
+resertEl.addEventListener('click', alertReset);
+buttonResetYEl.addEventListener('click', handlerButtonResetClick);
+buttonResetNEl.addEventListener('click', keepInfo);
