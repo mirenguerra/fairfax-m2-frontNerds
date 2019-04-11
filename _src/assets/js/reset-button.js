58b2483417932502215b85c5e5f6b1cc46d2bc3e
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-11 20:47:41 +0200

    Create functions for reset button

--- _src/assets/js/reset-button.js
+++ _src/assets/js/reset-button.js
@@ -1,4 +1,4 @@
-// El boton reset tiene que poner la paleta de colores en default, borrar todos los input del ormulario, borrar fotos miniatura y preview.
+// El boton reset tiene que poner la paleta de colores en default, borrar todos los input del ormulario, borrar fotos miniatura y preview. También tiene que dejar la tarjeta vacía y los iconos sin rodear.
 
 // Recoger el botón reset
 const buttonResetEl = document.querySelector('.reset__btn');
@@ -10,9 +10,47 @@ const formEl = document.querySelectorAll('.fillTheGap-input');
 const miniatureEl = document.querySelector('.profile-miniature');
 // Recoger la foto del preview
 const photoPreviewEl = document.querySelector('.card__picture');
+// Recoger user name de la tarjeta
+const userNameEl = document.querySelector('.card-user__name');
+// Recoger user job de la tarjeta
+const userJobEl = document.querySelector('.card-user__position');
+
+
+// Crear función que borre el form
+function resetInputs(){
+    for(let i=0;i<formEl.length;i++){
+        formEl[i].value = '';
+    }
+}
+
+// Crear función que borre user name y user job de la tarjeta
+function resetCard(){
+    userNameEl.innerHTML = 'User name';
+    userJobEl.innerHTML = 'User job';
+}
+
+// Crear función que borre los círculos de las rrss
+function resetRRSSCircles(){
+    const arrRRSSCircles = document.querySelectorAll('.card__item-list');
+    for (let i=0; i<arrRRSSCircles.length;i++){
+        arrRRSSCircles[i].classList.remove('card__item-list');
+        arrRRSSCircles[i].classList.add('card__item-list-no-border');
+    }
+}
+
+// Crear función que ponga el select en la posición default (el primer select)
 
 
 // Escuchar el click del botón reset
+buttonResetEl.addEventListener('click', handleButtonResetClick);
 
 // Poner un handler al click del botón reset
     // Cada value de cada elemento debe ser igual a ''
+function handleButtonResetClick(){
+    
+    resetInputs();
+    resetCard();
+    resetRRSSCircles();
+}
+
+
