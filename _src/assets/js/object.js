d601a3f6487f83ec0b014ea003d84129e7eb31c7
mirenguerra <miren.guerra.sanz@gmail.com>
2019-04-16 20:33:54 +0200

    Convert data to json

--- _src/assets/js/object.js
+++ _src/assets/js/object.js
@@ -1,6 +1,6 @@
 
 
-const userInfo = {
+let userInfo = {
   'palette': 1, //porque es el valor por defecto
   'name': '',
   'job': '',
@@ -12,14 +12,14 @@ const userInfo = {
 };
 
 //Funcion que guarda datos del objeto en localStorage
-
 function saveDataLs(){
     localStorage.setItem('userInfo', JSON.stringify(userInfo));
 };
-
-
-
 // Funcion que comprueba si hay datos en localStoragey con ellos hacer una autorrellenado
+function printDatafromLS(){
+    const userInfofromLS = localStorage.getItem('userInfo');    
+    userInfo = JSON.parse(userInfofromLS);
+    console.log(userInfo);  
+}
 
-
-
+printDatafromLS(); 
