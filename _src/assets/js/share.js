937a20dda2519d6554e993975b0cab276b1382ea
cterrasid <clarette.terrasi@gmail.com>
2019-04-17 14:00:23 +0200

    Add btn-twitter styles, try interactions unsuccessfully and improve footer logo to delete stress

---
+++ _src/assets/js/share.js
@@ -0,0 +1,9 @@
+const twitterBtnEl = document.querySelector('btn-twitter');
+const shareBtnEl = document.querySelector('btn-share');
+
+// function handleButtonClick(){
+//     twitterBtnEl.classList.toggle('hide__btn-twitter');
+    
+// }
+
+// shareBtnEl.addEventListener('click', handleButtonClick;
