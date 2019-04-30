2d2abc685c839bf551eb0d40e482bc9ad84891d2
carmen-tm <carmentorremolina@gmail.com>
2019-04-30 21:03:13 +0200

    Start refactoring the fill-form js

--- _src/assets/js/fill_form.js
+++ _src/assets/js/fill_form.js
@@ -1,21 +1,43 @@
+// function fillUserDataObject(key, value) {
+//   userData[key] = value;
+// }
 
+// // NAME AND JOB INPUTS
+// const inputName = document.querySelector('#name');
+// const cardUserName = document.querySelector('.card-user__name');
+
+// const inputJob = document.querySelector('#job');
+// const cardUserJob = document.querySelector('.card-user__position');
+
+// function handlerGetInfoKeyup(event) {
+//   const inputValue = event.currentTarget.value;
+//   const key = event.currentTarget.name;
+
+//   fillUserDataObject(key, inputValue);
+//   saveDataLs();
+
+//   // !userName ?
+// }
+
+// //Listener
+// inputName.addEventListener('keyup', handlerGetInfoKeyup);
 
 // NAME INPUT
-const setName = document.querySelector('#name');
+const inputName = document.querySelector('#name');
 const cardUserName = document.querySelector('.card-user__name');
 
 function getUserName() {
-  const userName = setName.value;
-  userInfo.name = userName;//llamo al nombre del objeto y a su elemento para sustituirlo en el object
+  const userName = inputName.value;
+  userInfo.name = userName; //llamo al nombre del objeto y a su elemento para sustituirlo en el object
   cardUserName.innerHTML = userName;
-  if (userName === '') {
-    cardUserName.innerHTML = 'User name';
-  }
-  saveDataLs()
+
+  !userName ? (cardUserName.innerHTML = 'User name') : '';
+
+  saveDataLs();
   console.log(userInfo);
 }
 
-setName.addEventListener('keyup', getUserName);
+inputName.addEventListener('keyup', getUserName);
 
 // JOB INPUT
 const setJob = document.querySelector('#job');
@@ -27,10 +49,9 @@ function getUserJob() {
   cardUserJob.innerHTML = userJob;
   if (userJob === '') {
     cardUserJob.innerHTML = 'User job';
-    
   }
 
- saveDataLs()
+  saveDataLs();
 }
 
 setJob.addEventListener('keyup', getUserJob);
@@ -39,13 +60,12 @@ setJob.addEventListener('keyup', getUserJob);
 const inputEmailEl = document.querySelector('#email');
 let linkEmailEl = document.querySelector('.email');
 
-
 function handlerEmail() {
   const inputValue = inputEmailEl.value;
   userInfo.email = inputValue;
   linkEmailEl.href = `mailto:${inputValue}`;
-    console.log(linkEmailEl);
-    saveDataLs();
+  console.log(linkEmailEl);
+  saveDataLs();
 }
 
 inputEmailEl.addEventListener('keyup', handlerEmail);
@@ -55,10 +75,10 @@ inputEmailEl.addEventListener('keyup', handlerEmail);
 const inputTel = document.getElementById('phone');
 const linkTel = document.getElementById('phone-link');
 function changeLinkTel() {
-linkTel.setAttribute('href', 'tel: +34' + inputTel.value);
-const telValue = inputTel.value;
-userInfo.phone = telValue;
-saveDataLs();
+  linkTel.setAttribute('href', 'tel: +34' + inputTel.value);
+  const telValue = inputTel.value;
+  userInfo.phone = telValue;
+  saveDataLs();
 }
 
 inputTel.addEventListener('keyup', changeLinkTel);
@@ -71,14 +91,13 @@ const iconStyle = document.querySelector('#gitHubEl');
 function getGitHub() {
   const userGitHub = setGitHub.value;
   userInfo.github = userGitHub;
- 
-  cardUserGitHub.setAttribute('target','_blank') ;  
+
+  cardUserGitHub.setAttribute('target', '_blank');
   if (userGitHub !== '') {
     iconStyle.classList.add('card__item-list-default');
     cardUserGitHub.href = `https://github.com/${userGitHub}`;
-  }
-  else{
-    cardUserGitHub.href ='';
+  } else {
+    cardUserGitHub.href = '';
   }
   console.log(userGitHub);
   saveDataLs();
