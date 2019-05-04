3b1f611e6ffd3fee52f434437f4290173e5e27aa
carmen-tm <carmentorremolina@gmail.com>
2019-05-04 20:38:34 +0200

    Re-refactor everything and clean code

--- _src/assets/js/fill_form.js
+++ _src/assets/js/fill_form.js
@@ -1,84 +1,40 @@
 const fillUserInfoObject = (element, key, value) => {
   key = element.name;
   value = userInfoToFill;
-
   userInfo[key] = value;
 };
 
 //REFACTORING 🚀
-// NAME & JOB INPUTS
-const fillNameAndJob = inputName => {
+const fillAllInputs = inputName => {
   const inputEl = document.querySelector(`#${inputName}`);
   const cardUserEl = document.querySelector(`.card-user__${inputName}`);
-  const myKey = inputName;
-
-  const handlerUserKeyup = event => {
-    const userInfoToFill = inputEl.value;
-    cardUserEl.innerHTML = userInfoToFill || `User ${myKey}`;
-
-    const element = event.currentTarget;
-    fillUserInfoObject(element, myKey, userInfoToFill);
-    saveDataLs();
-  };
-
-  inputEl.addEventListener('keyup', handlerUserKeyup);
-};
-fillNameAndJob('name');
-fillNameAndJob('job');
-
-/////////////////////////////////////////
-
-// EMAIL AND PHONE INPUTS
-const fillEmailAndPhone = inputName => {
-  const inputEl = document.querySelector(`#${inputName}`);
-  const cardUserEl = document.querySelector(`.card__link-${inputName}`);
+  const cardUserLinkEl = document.querySelector(`.card__link-${inputName}`);
   const myKey = inputName;
 
   const handlerUserKeyup = event => {
     const userInfoToFill = inputEl.value;
     const element = event.currentTarget;
-
-    if (myKey === 'email') {
+    if (myKey === 'name' || myKey === 'job') {
+      cardUserEl.innerHTML = userInfoToFill || `User ${myKey}`;
+    } else if (myKey === 'email') {
       userInfoToFill
-        ? cardUserEl.setAttribute('href', `mailto:${userInfoToFill}`)
-        : cardUserEl.setAttribute('href', '');
+        ? cardUserLinkEl.setAttribute('href', `mailto:${userInfoToFill}`)
+        : cardUserLinkEl.setAttribute('href', '');
     } else if (myKey === 'phone') {
       userInfoToFill
-        ? cardUserEl.setAttribute('href', `tel: +34${userInfoToFill}`)
-        : cardUserEl.setAttribute('href', '');
-    }
-    fillUserInfoObject(element, myKey, userInfoToFill);
-    saveDataLs();
-  };
-  inputEl.addEventListener('keyup', handlerUserKeyup);
-};
-
-fillEmailAndPhone('email');
-fillEmailAndPhone('phone');
-
-/////////////////////////////////////////
-
-//GITHUB AND LINKEDIN
-const fillGithubAndLinkedin = inputName => {
-  const inputEl = document.querySelector(`#${inputName}`);
-  const cardUserEl = document.querySelector(`.card__link-${inputName}`);
-  const myKey = inputName;
-
-  const handlerUserKeyup = event => {
-    const userInfoToFill = inputEl.value;
-    const element = event.currentTarget;
-
-    if (myKey === 'github') {
+        ? cardUserLinkEl.setAttribute('href', `tel: +34${userInfoToFill}`)
+        : cardUserLinkEl.setAttribute('href', '');
+    } else if (myKey === 'github') {
       userInfoToFill
-        ? cardUserEl.setAttribute(
+        ? cardUserLinkEl.setAttribute(
             'href',
             `https://github.com/${userInfoToFill}`
           )
-        : cardUserEl.setAttribute('href', '');
+        : cardUserLinkEl.setAttribute('href', '');
     } else if (myKey === 'linkedin') {
       userInfoToFill
-        ? cardUserEl.setAttribute('href', userInfoToFill)
-        : cardUserEl.setAttribute('href', '');
+        ? cardUserLinkEl.setAttribute('href', userInfoToFill)
+        : cardUserLinkEl.setAttribute('href', '');
     }
     fillUserInfoObject(element, myKey, userInfoToFill);
     saveDataLs();
@@ -86,36 +42,9 @@ const fillGithubAndLinkedin = inputName => {
   inputEl.addEventListener('keyup', handlerUserKeyup);
 };
 
-fillGithubAndLinkedin('github');
-fillGithubAndLinkedin('linkedin');
-
-////////////////////////////////////////
-
-// // GITHUB
-// const inputGitHubEl = document.querySelector('#gitHub');
-// const cardUserGitHub = document.querySelector('#gitHub-link');
-
-// const getGitHub = () => {
-//   const inputGithubValue = inputGitHubEl.value;
-//   userInfo.github = inputGithubValue;
-
-//   inputGithubValue !== ''
-//     ? (cardUserGitHub.href = `https://github.com/${inputGithubValue}`)
-//     : (cardUserGitHub.href = '');
-//   saveDataLs();
-// };
-// inputGitHubEl.addEventListener('keyup', getGitHub);
-
-// // USER LINKEDIN
-// const inputLinkedinEl = document.querySelector('#linkedin');
-// const cardUserLinkedinEl = document.querySelector('.card__link-linkedin');
-
-// const handleInputKeyup = () => {
-//   const linkedinInput = inputLinkedinEl;
-//   const inputValueLnkdIn = linkedinInput.value;
-
-//   userInfo.linkedin = inputValueLnkdIn;
-//   cardUserLinkedinEl.href = inputValueLnkdIn;
-//   saveDataLs();
-// };
-// inputLinkedinEl.addEventListener('keyup', handleInputKeyup);
+fillAllInputs('name');
+fillAllInputs('job');
+fillAllInputs('email');
+fillAllInputs('phone');
+fillAllInputs('github');
+fillAllInputs('linkedin');
