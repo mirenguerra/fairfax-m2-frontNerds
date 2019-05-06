9aa75569c741df6ba8f59f58b7734d6b7536f974
carmen-tm <carmentorremolina@gmail.com>
2019-05-06 20:54:09 +0200

    Prepare functions, waiting for merge to test

--- _src/assets/js/localStorage.js
+++ _src/assets/js/localStorage.js
@@ -1,16 +1,15 @@
-//Funcion que guarda datos del objeto en localStorage
 function saveDataLs() {
   localStorage.setItem('userInfo', JSON.stringify(userInfo));
 }
 
-// Funcion que comprueba si hay datos en localStorage y con ellos hacer una autorrellenado
+//Check if there is info on LS and auto-fill everything with that
 function printDatafromLS() {
   const userInfofromLS = localStorage.getItem('userInfo');
   if (userInfofromLS) {
     userInfo = JSON.parse(userInfofromLS);
     fillFormfromOBject();
     fillPaletteObject();
-    // fillCardfromObject();
+    fillCardfromObject();
   }
 }
 
@@ -20,12 +19,9 @@ function fillFormfromOBject() {
   document.getElementById('email').value = userInfo.email;
   document.getElementById('phone').value = userInfo.phone;
   document.getElementById('linkedin').value = userInfo.linkedin;
-  document.getElementById('gitHub').value = userInfo.github;
-  //   document.getElementById('github').value = userInfo.github;
+  document.getElementById('github').value = userInfo.github;
 }
 
-//esta funcion hace que se mantenga seleccionado el color de la paleta elegido.
-//cuando recarga la página si el color es paleta 1 se mantendra seleccionado el input y no variara por defecto.
 function fillPaletteObject() {
   if (userInfo.palette === 1) {
     inputGreen.checked = true;
@@ -36,57 +32,13 @@ function fillPaletteObject() {
   }
 }
 
-// function fillCardfromObject(){
-//     //Si userInfo.name está vacío,pone 'User name' igual para job
-//     cardUserName.innerHTML = userInfo.name||'User name';
-//     cardUserJob.innerHTML = userInfo.job||'User job';
-//     profileImage.style.backgroundImage = userInfo.photo;
-//     fillEmail();
-//     fillPhone();
-//     fillLinkedin();
-//     fillGitHub();
-// }
-
-// function fillEmail(){
-//     const emailCard = document.getElementById('email-li');
-//     const emailLink = document.querySelector('.email');
-//     if (inputEmailEl.value) {
-//         emailCard.classList.remove('card__item-list-no-border');
-//         emailCard.classList.add('card__item-list');
-//         emailLink.href = `mailto:${inputEmailEl.value}`;
-//     }
-// }
-
-// function fillPhone(){
-//     const phoneCard = document.getElementById('phone-li');
-//     if (inputTel.value) {
-//         linkTel.setAttribute('href', 'tel: +34' + inputTel.value);
-//         phoneCard.classList.remove('card__item-list-no-border');
-//         phoneCard.classList.add('card__item-list');
-//     }
-// }
-
-// function fillLinkedin(){
-//     const linkedinCard = document.getElementById('linkedin-li');
-//     if (linkedinInput.value) {
-//         linkedinCard.classList.remove('card__item-list-no-border');
-//         linkedinCard.classList.add('card__item-list');
-//         linkLnkdIn.href = linkedinInput.value;
-//     }
-// }
-
-// function fillGitHub(){
-//     const gitHubCard = document.getElementById('gitHubEl');
-//     const userGitHub = setGitHub.value;
-//     if (linkedinInput.value) {
-//         gitHubCard.classList.remove('card__item-list-no-border');
-//         gitHubCard.classList.add('card__item-list');
-//         cardUserGitHub.href = `https://github.com/${userGitHub}`;
-//         cardUserGitHub.setAttribute('target','_blank') ;
-//     }
-//     else {
-//         cardUserGitHub.href ='';
-//     }
-// }
+function fillCardfromObject() {
+  fillAllInputs('name');
+  fillAllInputs('job');
+  fillAllInputs('email');
+  fillAllInputs('phone');
+  fillAllInputs('github');
+  fillAllInputs('linkedin');
+}
 
 printDatafromLS();
