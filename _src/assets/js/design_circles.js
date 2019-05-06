f57ff486cff254bad8016700e7f46331d4fb3734
carmen-tm <carmentorremolina@gmail.com>
2019-05-06 23:11:47 +0200

    Fix bug at design_circles.js

--- _src/assets/js/design_circles.js
+++ _src/assets/js/design_circles.js
@@ -1,15 +1,18 @@
-//FUNCTIONS CIRCLES ON-OFF
-const handlerCirclesCard = event => {
-    const inputElement = event.currentTarget;
-    const circleElement = document.querySelector(`.${inputElement.id}`)
-
-    inputElement.value === '' 
-    ? ( circleElement.classList.add('card__item-list-no-border'), circleElement.classList.remove('card__item-list')) 
-    : (circleElement.classList.remove('card__item-list-no-border'), circleElement.classList.add('card__item-list'));
-}
-
 const inputsElementArr = document.querySelectorAll('.circle-item');
 
-for(let i = 0; i<inputsElementArr.length; i++){
-    inputsElementArr[i].addEventListener('keyup', handlerCirclesCard);
+//FUNCTIONS CIRCLES ON-OFF
+const handlerCirclesCard = event => {
+  const inputElement = event.currentTarget;
+  const circleElement = document.querySelector(`.${inputElement.id}`);
+  console.log(inputElement, circleElement, inputElement);
+
+  inputElement.value === ''
+    ? (circleElement.classList.add('card__item-list-no-border'),
+      circleElement.classList.remove('card__item-list'))
+    : (circleElement.classList.remove('card__item-list-no-border'),
+      circleElement.classList.add('card__item-list'));
+};
+
+for (let i = 0; i < inputsElementArr.length; i++) {
+  inputsElementArr[i].addEventListener('keyup', handlerCirclesCard);
 }
