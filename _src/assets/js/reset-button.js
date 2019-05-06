868df83a06cad25e74aef3179745fbaa87b72970
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-05-06 16:52:28 +0200

    clean code and arrow functions.working ok

--- _src/assets/js/reset-button.js
+++ _src/assets/js/reset-button.js
@@ -1,89 +1,50 @@
-// import { userInfo } from "os";
-
-// El boton reset abre un pop-up para borrar o no la información añadida hasta el momento.
-
-//El botón YES resetea el formulario, esto es ,la paleta de colores en default (verdes), borra todos los inputs, la foto miniatura y la foto de preview. También tiene que dejar la tarjeta vacía y los iconos sin rodear.
-
 /* eslint-disable strict */
-
 const buttonResetEl = document.querySelector('.reset__btn');
-// Recoge el select por defecto(el primero)
 const selectEl = document.querySelector('#palette--blue-green');
-
 const formEl = document.querySelectorAll('.fillTheGap-input');
-
-// const miniatureEl = document.querySelector('.profile-miniature');
-
-// const photoPreviewEl = document.querySelector('.card__picture');
-
 const userNameEl = document.querySelector('.card-user__name');
-
 const userJobEl = document.querySelector('.card-user__position');
-//Button Yes
 const buttonResetYEl = document.querySelector('.btn-resetY');
-//Button NO
 const buttonResetNEl = document.querySelector('.btn-resetN');
-//Pop Up
 const containerAlertEl = document.querySelector('.alert');
-
-//Listeners de los botones Reset,No y Yes, respectivamente
-buttonResetEl.addEventListener('click', alertReset);
-buttonResetNEl.addEventListener('click', keepInfo);
-buttonResetYEl.addEventListener('click', handlerButtonResetClick);
-
-//Muestra el pop up con las opciones YES y NO
-function alertReset() {
+const alertReset = () => {
   containerAlertEl.classList.add('alert-delete');
-}
-
-//Cuando pulsamos el botón NO el pop-up desaparece
-function keepInfo() {
+};
+buttonResetEl.addEventListener('click', alertReset);
+const keepInfo = () => {
   containerAlertEl.classList.remove('alert-delete');
-}
-
-// Función auxiliar que borra los inputs del form
-function resetInputs() {
+};
+buttonResetNEl.addEventListener('click', keepInfo);
+const resetInputs = () => {
   for (let i = 0; i < formEl.length; i++) {
     formEl[i].value = '';
   }
-}
-
-// Función auxiliar que vuelve a escribir el texto por defecto en la tarjeta
-function resetCard() {
+};
+const resetCard = () => {
   userNameEl.innerHTML = 'User name';
   userJobEl.innerHTML = 'User job';
-}
-
-// Función auxiliar que borra los círculos de las rrss
-function resetRRSSCircles() {
-  const arrRRSSCircles = document.querySelectorAll('.card__item-list');
-  for (let i = 0; i < arrRRSSCircles.length; i++) {
-    arrRRSSCircles[i].classList.remove('card__item-list');
-    arrRRSSCircles[i].classList.add('card__item-list-no-border');
+};
+const resetSN = () => {
+  const arrSN = document.querySelectorAll('.card__item-list');
+  for (let i = 0; i < arrSN.length; i++) {
+    arrSN[i].classList.remove('card__item-list');
+    arrSN[i].classList.add('card__item-list-no-border');
   }
-}
-
-// Función auxiliar que pone el select en la posición default (el primer select o verdes)
-function resetSelect() {
+};
+const resetColor = () => {
   selectEl.checked = true;
-  changePaletteGreen();
-}
-
-// Crear función para borrar miniatura y foto de preview
-function resetPhotos() {
+};
+const resetPhotos = () => {
   profileImage.style.backgroundImage = 'url("assets/images/darth-vader.jpg")';
   profilePreview.style.backgroundImage = `none`;
-  // miniatureEl.classList.remove('js__profile-preview');
-  // photoPreviewEl.classList.remove('js__profile-image');
-}
-
-// Función global que resetea todo el formulario cuando pulsamos el botón yes
-function handlerButtonResetClick() {
+};
+const handlerButtonResetClick = () => {
   resetInputs();
   resetCard();
-  resetRRSSCircles();
-  resetSelect();
+  resetSN();
+  resetColor();
   resetPhotos();
   containerAlertEl.classList.remove('alert-delete');
   localStorage.removeItem('userInfo');
-}
+};
+buttonResetYEl.addEventListener('click', handlerButtonResetClick);
