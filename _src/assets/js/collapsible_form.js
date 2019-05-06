b3c966ee495795005580502e5dd3d5c848993505
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-06 20:21:21 +0200

    improve with arrow function

--- _src/assets/js/collapsible_form.js
+++ _src/assets/js/collapsible_form.js
@@ -2,16 +2,18 @@
 
 const legendTitles = document.querySelectorAll('.fieldset__title__container');
 
-for (const legend of legendTitles){
-    legend.addEventListener('click', handlerLegendClick);
-}
-
-function handlerLegendClick(event){
+const handlerLegendClick = event =>{
     const element = event.currentTarget;
     const currentPanel = element.nextElementSibling;
     const currentArrow = element.querySelector('.arrow');
     currentPanel.classList.toggle('hidden');
     currentArrow.classList.toggle('fa-chevron-up');
     currentArrow.classList.toggle('fa-chevron-down');
+};
+
+for (const legend of legendTitles){
+    legend.addEventListener('click', handlerLegendClick);
 }
+
+
 
