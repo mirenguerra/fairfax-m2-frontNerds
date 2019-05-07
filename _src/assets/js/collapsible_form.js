c1e5f01ae65b162e2f29764a2da9f795f08c14bb
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-07 10:51:00 +0200

    close all collapsibles when click one

--- _src/assets/js/collapsible_form.js
+++ _src/assets/js/collapsible_form.js
@@ -1,14 +1,34 @@
 // COLLAPSABLE SECTIONS
 
 const legendTitles = document.querySelectorAll('.fieldset__title__container');
+const panels = document.querySelectorAll('.panel');
+const arrows =document.querySelectorAll('.arrow');
 
 const handlerLegendClick = event =>{
+    
     const element = event.currentTarget;
     const currentPanel = element.nextElementSibling;
     const currentArrow = element.querySelector('.arrow');
-    currentPanel.classList.toggle('hidden');
-    currentArrow.classList.toggle('fa-chevron-up');
-    currentArrow.classList.toggle('fa-chevron-down');
+    if(currentArrow.classList.contains('fa-chevron-down')){
+        for (let i=0; i<legendTitles.length; i++) {
+            panels[i].classList.add('hidden');
+            arrows[i].classList.add('fa-chevron-down');
+            arrows[i].classList.remove('fa-chevron-up');
+
+        }
+    currentPanel.classList.remove('hidden');
+    currentArrow.classList.remove('fa-chevron-down');
+    currentArrow.classList.add('fa-chevron-up');
+    }
+    else {
+        currentPanel.classList.add('hidden');
+        currentArrow.classList.add('fa-chevron-down');
+        currentArrow.classList.remove('fa-chevron-up');
+    }
+    
+  
+  
+    
 };
 
 for (const legend of legendTitles){
