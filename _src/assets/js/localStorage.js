32b7709230910c93955f29b5069caa48765db1f2
carmen-tm <carmentorremolina@gmail.com>
2019-05-07 14:09:07 +0200

    Refactor converting all funcitons into arrow ones

--- _src/assets/js/localStorage.js
+++ _src/assets/js/localStorage.js
@@ -1,9 +1,9 @@
-function saveDataLs() {
+const saveDataLs = () => {
   localStorage.setItem('userInfo', JSON.stringify(userInfo));
-}
+};
 
 //Check if there is info on LS and auto-fill everything with that
-function printDatafromLS() {
+const printDatafromLS = () => {
   const userInfofromLS = localStorage.getItem('userInfo');
   if (userInfofromLS) {
     userInfo = JSON.parse(userInfofromLS);
@@ -14,8 +14,9 @@ function printDatafromLS() {
     //THE CIRCLES FUNCTIONALITY IS NOT WORKING YET
     updateCirclesWithUserInfo();
   }
-}
-function fillPaletteObject() {
+};
+
+const fillPaletteObject = () => {
   if (userInfo.palette === 1) {
     inputGreen.checked = true;
   } else if (userInfo.palette === 2) {
@@ -23,19 +24,19 @@ function fillPaletteObject() {
   } else {
     inputGrey.checked = true;
   }
-}
+};
 
 //All the following code could be simplified by using the Object.keys() method on the object, with returns an array of its keys, and just iterating it with two bucles.
-function fillFormfromOBject() {
+const fillFormfromOBject = () => {
   document.getElementById('name').value = userInfo.name;
   document.getElementById('job').value = userInfo.job;
   document.getElementById('email').value = userInfo.email;
   document.getElementById('phone').value = userInfo.phone;
   document.getElementById('linkedin').value = userInfo.linkedin;
   document.getElementById('github').value = userInfo.github;
-}
+};
 
-function fillCardfromObject() {
+const fillCardfromObject = () => {
   drawInfoPreviewCard('name', userInfo.name);
   drawInfoPreviewCard('job', userInfo.job);
   drawInfoPreviewCard('email', userInfo.email);
@@ -45,6 +46,6 @@ function fillCardfromObject() {
 
   profileImage.style.backgroundImage = `url(${userInfo.photo})`;
   profilePreview.style.backgroundImage = `url(${userInfo.miniature})`;
-}
+};
 
 printDatafromLS();
